7dc8f34cb97fe903d178e7decb1649c044945599
nitefury7 <ayush.infinity5@gmail.com>
2024-12-16 13:44:39 +0545

    fix: json data sturucture

--- migrate.py
+++ migrate.py
@@ -26,7 +26,7 @@ def login_to_source(base_url, username, password):
 
 def fetch_document_list(doctype, cookies):
     """Fetch the list of document names from the source Frappe instance."""
-    response = requests.get(f"{config.SOURCE_URL}/api/resource/{doctype}", cookies=cookies)
+    response = requests.get(f"{config.SOURCE_URL}/api/resource/{doctype}", cookies=cookies, params=config.params)
     if response.status_code == 200:
         return [entry["name"] for entry in response.json().get("data", [])]
     else:
@@ -50,18 +50,18 @@ def fetch_data(doctype, name, cookies):
         return {}
 
 
-def send_data(doctype, name, data):
+def send_data(doctype, data):
     """Send a specific document's data to the destination Frappe instance."""
     response = requests.post(
-        f"{config.DESTINATION_URL}/api/resource/{doctype}/{name}",
+        f"{config.DESTINATION_URL}/api/resource/{doctype}",
         headers=destination_headers,
         json={"data": data},
     )
     if response.status_code == 200:
-        print(f"Successfully sent record: {name}")
+        print(f"Successfully sent record: {data.get("name")}")
     else:
         print(
-            f"Failed to send record: {name}. Status Code: {response.status_code}, Response: {response.text}"
+            f"Failed to send record: {data.get("name")}. Status Code: {response.status_code}, Response: {response.text}"
         )
 
 
@@ -73,13 +73,12 @@ if __name__ == "__main__":
     if names:
         print(f"Fetched {len(names)} document names:\n {names}")
         for name in names:
-            print(f"Processing {name}...")
             data = fetch_data(doctype=config.doctype, name=name, cookies=source_cookies)
-            print(data)
-            # if data:
-            #     # print(data)
-            #     send_data(doctype=config.doctype, name=name, data=data)
-            # else:
-            #     print(f"Skipping {name} due to fetch error.")
+            # print(data)
+            if data:
+                data["supplier_type"]="Company"
+                send_data(doctype=config.doctype, data=data)
+            else:
+                print(f"Skipping {name} due to fetch error.")
     else:
         print("No document names to process.")
